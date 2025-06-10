cb2e2b8683daf21411b69b66390a8acf50578a44
Amirhossein <amirmemool12@gmail.com>
2025-06-10 20:06:45 +0330

    add server to fx

--- cmd/first-app/main.go
+++ cmd/first-app/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"event-collector/internal/config"
 	"event-collector/internal/database"
+	"event-collector/internal/transport/http"
 	"fmt"
+	"github.com/gofiber/fiber/v3"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/fx"
 )
@@ -15,10 +17,11 @@ func main() {
 		fx.Provide(
 			config.NewConfig,
 			database.NewMongo,
+			http.NewHTTPServer,
 		),
 		// Invoke is used for functions that are needed for their side effects,
 		// but don't provide any new types. This is our main application logic.
-		fx.Invoke(runApplication),
+		fx.Invoke(func(*fiber.App) {}, runApplication),
 	)
 
 	// Run the application. This call is blocking.
