04cf7d963cabb3a08779933284a815ea014a5e5e
Amirhossein <amirmemool12@gmail.com>
2025-06-10 18:16:44 +0330

    use fix

--- cmd/first-app/main.go
+++ cmd/first-app/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
-	"context"
+	"event-collector/internal/config"
 	"event-collector/internal/database"
-	"event-collector/internal/greeting"
 	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.uber.org/fx"
 )
 
 func main() {
+	app := fx.New(
+		// Provide all of our constructors. FX will call them in the
+		// right order and handle their lifecycle.
+		fx.Provide(
+			config.NewConfig,
+			database.NewMongo,
+		),
+		// Invoke is used for functions that are needed for their side effects,
+		// but don't provide any new types. This is our main application logic.
+		fx.Invoke(runApplication),
+	)
 
-	// 💡 Best practice: Get MongoDB URI from an environment variable.
-	db := database.GetMongo(context.Background())
+	// Run the application. This call is blocking.
+	// It will only return when the application is shutting down.
+	app.Run()
+}
 
-	message := greeting.Hello("first app")
-	fmt.Println(message)
-
-	db.Close(context.Background())
+func runApplication(client *mongo.Client) {
+	fmt.Println("Starting application...")
 }
