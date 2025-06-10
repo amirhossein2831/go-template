6b0cfc87814882635f1fef6dd4187ec87c38c333
Amirhossein <amirmemool12@gmail.com>
2025-06-10 19:34:08 +0330

    write a generic parser

---
+++ pkg/parse/variable-parser.go
@@ -0,0 +1,41 @@
+package parse
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func ToPrimary[T string | int | int64 | float64 | bool](value string) (T, error) {
+	var zero T
+
+	switch any(zero).(type) {
+	case string:
+		return any(value).(T), nil
+	case int:
+		i, err := strconv.Atoi(value)
+		if err != nil {
+			return zero, err
+		}
+		return any(i).(T), nil
+	case int64:
+		i64, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return zero, err
+		}
+		return any(i64).(T), nil
+	case float64:
+		f64, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			return zero, err
+		}
+		return any(f64).(T), nil
+	case bool:
+		b, err := strconv.ParseBool(value)
+		if err != nil {
+			return zero, err
+		}
+		return any(b).(T), nil
+	default:
+		return zero, fmt.Errorf("unsupported type for conversion: %T", zero)
+	}
+}
