b1c3044b4ea97b9e4f1ea6ac27ee6af6e87078a6
Amirhossein <amirmemool12@gmail.com>
2025-06-11 13:14:55 +0330

    create handler

---
+++ internal/transport/grpc/handler/greeting-handler.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"context"
+	"event-collector/internal/service"
+	providev1 "event-collector/pkg/grpc/provide/v1"
+	"log"
+)
+
+// GreetingHandler implements the gRPC server interface for the GreetingService.
+type GreetingHandler struct {
+	providev1.UnimplementedGreetingServiceServer
+	greetingService *service.GreetingService
+}
+
+// NewGreetingHandler is the constructor for our gRPC handler.
+func NewGreetingHandler(gs *service.GreetingService) *GreetingHandler {
+	return &GreetingHandler{
+		greetingService: gs,
+	}
+}
+
+// SayGreeting is the implementation of our gRPC RPC.
+func (h *GreetingHandler) SayGreeting(ctx context.Context, req *providev1.SayGreetingRequest) (*providev1.SayGreetingResponse, error) {
+	log.Printf("gRPC transport: Received SayGreeting request: Name=%s", req.Name)
+
+	greeting, err := h.greetingService.GenerateGreetingLogic(ctx, req.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &providev1.SayGreetingResponse{GreetingMessage: greeting}, nil
+}
